dab6633cf5577f7f67ec12759f7a9818b8499754
naohr <najmim625@gmail.com>
2023-02-04 09:39:14 +0700

    update: extend Request interface

--- src/interfaces/global.interface.ts
+++ src/interfaces/global.interface.ts
@@ -1,4 +1,19 @@
+import { Request } from "express";
+import { ICertificate } from "../models/Cert.model";
+import { IDetailSchema } from "../models/Detail.model";
+import { IPrivateMessage } from "../models/PrivateMessage.model";
+import { IProject } from "../models/Project.model";
+import { ISkill } from "../models/Skill.model";
+
 export interface IUserLogin {
     username: String
     password: String
+}
+
+export interface IRequest extends Request {
+    userCred : {
+        uid: string,
+        jwt: string
+    },
+    bucket : ICertificate | IProject | ISkill | IDetailSchema | IPrivateMessage | IDetailSchema | null
 }
