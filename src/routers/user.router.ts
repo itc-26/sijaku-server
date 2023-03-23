47b3d93e25cd4ec6d62b3c14427a8c9a3b4f9903
naohr <najmim625@gmail.com>
2023-03-23 16:41:41 +0700

    update: user router

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,6 +1,16 @@
-import express from "express";
+import express, {RequestHandler} from "express";
+import {authenticate} from "../middlewares/auth.middleware";
+import { strictBelongsTo } from "../middlewares/util.middleware";
+import { me, post, get, edit } from "../controllers/user.controller";
 
 const userRouter = express.Router();
+
+userRouter.post("/post/:type", authenticate as RequestHandler, strictBelongsTo , post);
+
+userRouter.get("/me", authenticate as RequestHandler, me);
+userRouter.get("/get/:type", authenticate as RequestHandler, get);
+
+userRouter.put("/edit/:type/:id", authenticate as RequestHandler, strictBelongsTo ,edit);
 
 export default userRouter;
 
