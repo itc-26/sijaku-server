7ec66b710005040f3444e58757e4a127ac70d522
naohr <najmim625@gmail.com>
2023-03-23 16:37:58 +0700

    update: minor change

--- src/models/Project.model.ts
+++ src/models/Project.model.ts
@@ -1,43 +1,7 @@
-// import mongoose, { Schema, model, Document } from "mongoose";
-
-// export interface IProject extends Document{
-//     belongsTo: mongoose.Types.ObjectId,
-//     name: String,
-//     description: String,
-//     link: String
-// }
-
-
-// const ProjectSchema = new Schema<IProject>({
-//     belongsTo : {
-//         type : Schema.Types.ObjectId,
-//         ref : "User",
-//         required : true
-//     },
-//     name : {
-//         type : String,
-//         required : true,
-//         maxLength : 40
-//     },
-//     description : {
-//         type : String,
-//         required : true,
-//         maxLength: 255
-//     },
-//     link : {
-//         type : String,
-//         required : true,
-//     }
-// })
-
-// const UserProject = mongoose.model<IProject>("Project", ProjectSchema);
-
-// export default UserProject;
-
-import mongoose, { Schema, model, Document } from "mongoose";
+import { Schema, model, Document, Types } from "mongoose";
 
 export interface IProject extends Document {
-    belongsTo: mongoose.Types.ObjectId,
+    belongsTo: Types.ObjectId,
     name: String,
     description: String,
     link: String
